9a15615ae9752ac94ee503b723705eae2d5b1a76
EyadMamoun <eyadmamoun41@gmail.com>
2025-02-23 13:29:08 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(window).scroll(function(){
+    let windowScroll = $(window).scrollTop();
+    let aboutTop = $("#ABOUT").offset().top;
+    if(windowScroll >= aboutTop)
+    {
+        $("nav").css("backgroundColor","rgba(0, 0, 0, 0.7)");
+        $("#upBtn").fadeIn(500);
+    }
+    else
+    {
+        $("nav").css("backgroundColor","transparent");
+        $("#upBtn").fadeOut(500);
+    }
+});
+
+$("a[href^='#']").click(function(eventInfo){
+    let aHref = $(eventInfo.target).attr('href');
+    let aboutTop = $(aHref).offset().top;
+    $("html,body").animate({scrollTop:aboutTop},1000);
+})
